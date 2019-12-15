64002016e9c80a402a5a5df6ea8d41ada7b3b6c9
Ian Buttimer <x14112451@student.ncirl.ie>
2019-12-15 22:30:31 +0000

    fix typo

--- run_demo.sh
+++ run_demo.sh
@@ -3,5 +3,5 @@
 export POSTGRES_CFG=demo_postgres.cfg
 export CSV_DIR_PATH=data/apache_logs
 
-#python3 -q main.py cfg_path=demo_config.yaml plot_path=plots.yaml data_dir=data/sales_journal orca_exe=/home/ibuttimer/anaconda3/lib/orca_app/orca
-python3 -q main.py cfg_path=demo_config.yaml plot_path=plots.yaml data_dir=data/sales_journal
+#python3 -q main.py --cfg_path=demo_config.yaml --plot_path=plots.yaml --data_dir=data/sales_journal --orca_exe=/home/ibuttimer/anaconda3/lib/orca_app/orca
+python3 -q main.py --cfg_path=demo_config.yaml --plot_path=plots.yaml --data_dir=data/sales_journal
